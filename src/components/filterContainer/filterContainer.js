68a19f0b393f1750d46c4a8a2680ab2da63a1978
ArunprakashLfs <arunprakash7871@gmail.com>
2023-03-29 21:34:04 +0530

    added fonts and sort and search-bar

--- src/components/filterContainer/filterContainer.js
+++ src/components/filterContainer/filterContainer.js
@@ -1,8 +1,19 @@
+import './filtercontainer.style.css';
+// import {faSearch} from 'react-icons/fa';
 const FilterContainer = ()=>{
     return(
         <>
-        <input className="filter-container" />
+        <div className='search-container'>
+            <div className='filter-container'>
+                <section className='searchbar'>
+                <span className='search-icon'><i class="fa-solid fa-magnifying-glass" ></i></span>
+                <input className='search-text'></input>
+                </section>
+                <span className='sort-icon'><i class="fa-solid fa-sort" ></i></span>
+            </div>
+            
+        </div>
         </>
     )
-}
+};
 export default FilterContainer;
