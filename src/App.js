e735b0cb4c9317f0eafd7067b38cc4eaad2f88bd
kesavan <arunprakash7871@gmail.com>
2023-03-30 07:41:49 -0400

    add sort

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import Products from './components/products/products';
 import FilterContainer from './components/filterContainer/filterContainer';
+import productData from './components/productData/productData';
+// import ProductData from './components/productData/productData';
 
-const sortValue= ()=>{
-
-}
+ 
 function App() {
+  // let productData = [...ProductData]
+  let [products,setproducts] = useState(productData);
+  let [isasending, setisasending] = useState(false);
+ 
+  function sortValue(){
+    let copyData = [...productData];
+    copyData.sort((val1, val2)=>{
+      if(!isasending){
+        return(
+          val1.price- val2.price
+        ) 
+      }else{
+        return(
+          val2.price-val1.price
+        )
+      }
+    });
+    isasending = setisasending;
+    productData =setisasending;
+  }
+  
   return (
     <>
     <h1>My Products</h1>
-    <FilterContainer
-     sortValue/>
-    <Products />
+    <FilterContainer  sortValue={sortValue}/>
+    <Products  productData={productData}/>
     </>
   );
 }
--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,26 +1,27 @@
-import ProductData from'../productData/productData';
+// import ProductData from'../productData/productData';
 import '../products/products.css';
 // import FilterContainer from '../filterContainer/filterContainer';
-const Products = ()=>{
+const Products = ({ productData }) => {
+    console.log(productData);
+    
     return(
         <>
         <div className='container'>
-        
-            
-            {ProductData.map(({title, price, images, description, rating, category})=>{
+            {/* console.log(productData); */}
+            {productData.map((val)=>{
                 return(
                 <main className="card">
                     <div className='img-container'>
-                        <img src={images[0]} alt="phone_image" className='img'></img>
+                        <img src={val.images[0]} alt="phone_image" className='img'></img>
                     </div>
                     <div className='text-container'>
-                        <h2>{title}</h2>
-                        <h3>{category}</h3>
+                        <h2>{val.title}</h2>
+                        <h3>{val.category}</h3>
                         {/* <span>{thumbnail}</span> */}
-                        <h3>price: ${price}</h3>
-                        <small>{rating}⭐</small>
+                        <h3>price: ${val.price}</h3>
+                        <small>{val.rating}⭐</small>
                         <br/>
-                        <p>{description}</p>
+                        <p>{val.description}</p>
                     </div>
                 </main>
             )
