3cecceee99244377000ff3b2a7dadc8681a357db
kesavan <arunprakash7871@gmail.com>
2023-03-30 03:34:15 -0400

    added form datas

--- src/components/filterContainer/filterContainer.js
+++ src/components/filterContainer/filterContainer.js
@@ -8,10 +8,11 @@ const FilterContainer = (sortValue)=>{
         <>
         <div className='search-container'>
             <div className='filter-container'>
-                <section className='searchbar'>
+                <form className='searchbar'>
                 <input className='search-text'></input>
                 <button className='search-icon'><i class="fa-solid fa-magnifying-glass" ></i></button>
-                </section>
+                </form>
+
                 <button className='sort-icon' onClick={sortValue}><i class="fa-solid fa-sort" ></i></button>
             </div>
             
